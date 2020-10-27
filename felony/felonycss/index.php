5fab9b50c6f13c7de44fd86c41e383aebf3c0fd1
ozcandongel <ozcandongel35@gmail.com>
2020-10-27 23:12:32 +0300

    gtyhuj

---
+++ felony/felonycss/index.php
@@ -0,0 +1,74 @@
+<style type="text/css">
+.felonybilgi{
+    font-family:sans-serif; 
+    font-size:15px; 
+    color: white; 
+    line-height:20px; 
+    font-weight:400;
+}
+.felonyppresim{
+	border-radius:50%;
+	width:100px;
+	max-width:99%;
+
+}
+#felonylist{ 
+    display:inline-block;
+} 
+#felonylink{
+text-decoration:none; color:#003569; font-family:sans-serif; font-size:13px; font-weight:540; vertical-align: baseline; 
+}
+#felonybuton{
+    color:white; width:80%; outline:0; border:0; border-radius:3px;   
+    background: #3897f0;
+    border-color: #3897f0;    
+    font-size: 14px; 
+    font-weight:bold; 
+    height:33px;
+    width:250px;
+}
+#felonybuton1{
+    color:white; width:80%; outline:0; border:0; border-radius:3px;   
+    background: #3897f0;
+    border-color: #3897f0;    
+    font-size: 14px; 
+    font-weight:bold; 
+    height:33px;
+    width:50px;
+}
+#felonymetin{
+    font-family:sans-serif; 
+    font-size:15px; 
+    color: white; 
+    line-height:20px; 
+    font-weight:400;
+}
+#felonykutu{
+    padding:6px; 
+    background:#fafafa; 
+    outline:none;
+    width:250px; 
+    height:44px; 
+    border:5px solid #dedede; 
+    font-family:sans-serif; 
+    font-weight:350;
+    flex: 1 0 0px;
+    margin: 0;
+    outline: 0;
+    overflow: hidden;
+    padding: 9px 0 7px 8px;
+    text-overflow: ellipsis;
+    border: 1px solid #e6e6e6;    
+    text-overflow: ellipsis;
+    font: 400 13.3333px Arial; 
+    border-radius:3px;
+}
+#felonyaltmetin{
+    font-family:sans-serif; 
+    font-size:14px; 
+    color:#999;
+}
+.body{
+   background: #fafafa; 
+}
+</style>
